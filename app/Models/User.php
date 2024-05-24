284cee7d9d7779c1bd3c6d916cfaab9a3f3ea634
Syed Numan <snahmed20017@gmail.com>
2024-05-24 02:08:14 +0200

    install filament and create User Module with adding role feild

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Panel;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Jetstream\HasProfilePhoto;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens;
     use HasFactory;
@@ -18,14 +20,36 @@ class User extends Authenticatable
     use Notifiable;
     use TwoFactorAuthenticatable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    const Role_Admin = 'ADMIN';
+    const Role_Laibrarian = 'LAIBRARIAN';
+    const Role_Student = 'STUDENT';
+    const Role_Default = self::Role_Student;
+
+    const Roles = [
+        self::Role_Admin => "Admin",
+        self::Role_Laibrarian => "Laibrarian",
+        self::Role_Student => "Student",
+    ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->isAdmin() || $this->isLaibrarian();
+    }
+  
+    public function isAdmin(){
+        return $this->role === self::Role_Admin;
+    }
+    public function isLaibrarian(){
+        return $this->role === self::Role_Laibrarian;
+    }
+    
+    public function isStudent(){
+        return $this->role === self::Role_Student;
+    }
     protected $fillable = [
         'name',
         'email',
+        'role',
         'password',
     ];
 
